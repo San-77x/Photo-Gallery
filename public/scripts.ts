2799961a6260724eb2f4177f36f9c2fae854c309
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-01-25 13:52:37 +0530

    feat: add typescript for response

---
+++ public/scripts.ts
@@ -0,0 +1,13 @@
+const input = document.getElementById("search") as HTMLInputElement | null;
+const button = document.getElementById("searchButton");
+const Apikey = "42008578-85a7f1a17bdb7be2e2f29df16";
+const ApiUrl = "https://pixabay.com/api/";
+
+if (button)
+  button.onclick = () => {
+    if (input && input.value === "") {
+      alert("Type anything First");
+      return;
+    }
+    fetch("ApiUrl");
+  };
