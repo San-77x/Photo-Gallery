0c27d3b143a61b326ae35fc7db806711fd868a91
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-01-25 14:33:31 +0530

    feat:add search for images

---
+++ public/script.js
@@ -0,0 +1,16 @@
+const input = document.getElementById("search");
+const button = document.getElementById("searchButton");
+const Apikey = "42008578-85a7f1a17bdb7be2e2f29df16";
+const ApiUrl = "https://pixabay.com/api/?key=";
+const webFormatUrl =
+  ApiUrl + Apikey + "&q=" + input.textContent + "&image_type=photo&pretty=true";
+
+button.onclick = () => {
+  if (input.value === "") {
+    alert("Type anything First");
+    return;
+  }
+  fetch("webFormatUrl");
+  console.log(webFormatUrl);
+};
+console.log(webFormatUrl);
--- public/scripts.ts
+++ public/scripts.ts
@@ -1,7 +1,8 @@
 const input = document.getElementById("search") as HTMLInputElement | null;
 const button = document.getElementById("searchButton");
 const Apikey = "42008578-85a7f1a17bdb7be2e2f29df16";
-const ApiUrl = "https://pixabay.com/api/";
+const ApiUrl = "https://pixabay.com/api/?key";
+const webFormatUrl = ApiUrl + Apikey + "&q=" + input + "&image_type=photo";
 
 if (button)
   button.onclick = () => {
@@ -9,5 +10,5 @@ if (button)
       alert("Type anything First");
       return;
     }
-    fetch("ApiUrl");
+    fetch("webFormatUrl");
   };
